5f76e1ab0ff2d4c10c518a2dc09dada63324c56c
harmeet27 <harmeetkaur2793@gmail.com>
2018-07-30 17:42:07 +0530

    css

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-    <div>
+    <div className='Person'>
     <button onClick={props.click}> change from person</button>
     <p>I am a person my name is {props.name} and age is {props.age}</p>
     <p>{props.children}</p>
