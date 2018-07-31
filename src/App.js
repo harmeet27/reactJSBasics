bf026e615a8bdebb88dc85ce9743e0d3d25d836c
harmeet27 <harmeetkaur2793@gmail.com>
2018-07-31 21:25:07 +0530

    dynamic elements

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ class App extends Component {
       {
         name:'anu', age:'18'
       }
-    ]
+    ],
+
+    defaultShow : false
+
    
   }
 
@@ -45,23 +48,44 @@ class App extends Component {
 
   }
 
+  toggleName = () => {
+    const defaultShowconst = this.state.defaultShow;
+    this.setState({defaultShow : !defaultShowconst});
+
+  }
+
   render() {
 
    const style ={
      backgroundColor : 'grey',
      font : 'inherit',
      border : '5px solid black',
-     padding : '8px'
+     padding : '8px',
+     cursor: 'pointer'
 
    };
+
+   let persons = null;
+
+   if(this.state.defaultShow)
+   {
+     persons = (
+       <div>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} 
+           click={this.switchName.bind(this,'Ankita')} nameChange={this.nameChange}>children are</Person>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >anu is</Person>
+      </div>
+     );
+
+   }
+
 
     return (
       <div className="App">
         <h1>My react app </h1>
-        <button style={style} onClick={() => this.switchName('Faran')}>Change name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} 
-        click={this.switchName.bind(this,'Ankita')} nameChange={this.nameChange}>children are</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >anu is</Person>
+        {/* <button style={style} onClick={() => this.switchName('Faran')}>Change name</button> */}
+        <button style={style} onClick={this.toggleName}>Change name</button> 
+        {persons}
       </div>
     );
   }
